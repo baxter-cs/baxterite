0220fad4c5d0e7859ff14c6cd19afe2b4f75b125
JCharante <jeanclaude.zarate@baxter-academy.org>
2016-06-04 08:58:21 -0400

    added user table

--- src/server/database_setup.py
+++ src/server/database_setup.py
@@ -1,6 +1,6 @@
 import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+import sys, datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -31,17 +31,37 @@ class ClassName(Base):
     active = Column(Boolean, nullable=False)
     name = Column(String(), nullable=False)
 
-
-# Couldn't come up for a better name for a class session.
+# Couldn't come up for a bett
+er name for a class session.
 class Instance(Base):
     __tablename__ = 'Instance'
     instance_id = Column(Integer, primary_key=True)
     class_id = Column(Integer, nullable=False)
     className_id = Column(Integer, nullable=False)
     block = Column(String(), nullable=False)
-    start_trimester = Column(Integer, nullable=False)
-    end_trimester = Column(Integer, nullable=False)
-    year = Column(Integer, nullable=False)
+    start_trimester = Column(Integer, nullable=False)  # 1 for first trimester in a school year, 2 for second...
+    end_trimester = Column(Integer, nullable=False)  # For most instances this will be the same as the start_trimester
+    year = Column(Integer, nullable=False)  # The start of the school year (2015 for 2015-2016 school year
+
+
+class InstanceMember(Base):
+    __tablename__ = 'InstanceMember'
+    instance_member_id = Column(Integer, primary_key=True)
+    instance_id = Column(Integer, nullable=False)  # The id of the instance
+    student_id = Column(Integer, nullable=False)
+
+
+class Student(Base):
+    __tablename__ = 'Student'
+    student_id = Column(Integer, primary_key=True)
+    email = Column(String(), nullable=False, unique=True)
+    name = Column(String(), nullable=True)
+    avatar = Column(String())
+    active = Column(Boolean, default=False)
+    tokens = Column(Text)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow())
+
+
 
 
 # Create an engine that stores data in the local directory's
